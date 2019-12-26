a5520a864b0f909b9aa8d83101c37860bf847490
Artur <artmonkey789@gmail.com>
2019-12-26 15:56:10 +0600

    Rendering exceptions like json by default

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,23 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
 use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that are not reported.
      *
@@ -46,6 +58,63 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if($exception instanceof HttpResponseException){
+            return $exception->getResponse();
+        }
+
+        if($exception instanceof AuthenticationException){
+            return $this->unauthenticated($request, $exception);
+        }
+
+        if($exception instanceof AuthorizationException){
+            $this->errorResponse($exception->getMessage(), JsonResponse::HTTP_FORBIDDEN);
+        }
+
+        if($exception instanceof ValidationException){
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+
+        if($exception instanceof ModelNotFoundException){
+            $modelName = strtolower(class_basename($exception->getModel()));
+
+            return $this->errorResponse("Does not exist any {$modelName} with specified identification", JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        if($exception instanceof NotFoundHttpException){
+            return $this->errorResponse('The specified URL cannot be found', JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        if($exception instanceof MethodNotAllowedHttpException){
+            $this->errorResponse('The specified method for the request is invalid',JsonResponse::HTTP_METHOD_NOT_ALLOWED);
+        }
+
+        if($exception instanceof HttpException){
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        if($exception instanceof QueryException){
+            $errorCode = $exception->errorInfo[1];
+
+            if($errorCode == 1451){
+                return $this->errorResponse('Cannot remove this resource permanently. It\'s related with any other resource', JsonResponse::HTTP_CONFLICT);
+            }
+        }
+        if(config('app.debug')){
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Unexpected exception. Try later', JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('Unauthenticated', JsonResponse::HTTP_UNAUTHORIZED);
+    }
+
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        $errors = $e->validator->errors()->getMessages();
+
+        return $this->errorResponse($errors,JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
     }
 }
